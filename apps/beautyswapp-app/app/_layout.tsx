1cad511bc93c66a586730506588c53f685bfff6d
Lionel B <lionel95200@gmail.com>
2026-02-18 21:47:54 +0100

    feat: add sentry

--- apps/beautyswapp-app/app/_layout.tsx
+++ apps/beautyswapp-app/app/_layout.tsx
@@ -1,6 +1,21 @@
 import 'react-native-reanimated';
 import { AppProviders } from '@/components/AppProviders';
+import * as Sentry from '@sentry/react-native';
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: 'https://c45f2bd955f599742c3bf1dc0013a20d@o4510749955129344.ingest.de.sentry.io/4510908885172304',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
   return <AppProviders />;
-}
+});
